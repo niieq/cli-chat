939e0c424fe0b989129291ec63787bfa014fe017
Nii Equity <nii.nmarley@gmail.com>
2017-01-08 08:10:12 +0000

    Send message to specific client, Minor fixes

--- server.py
+++ server.py
@@ -14,13 +14,13 @@ class ChatServer:
         self.sockserver.listen(5)
 
         self.connectors = {}
-        self.connectors['server'] = self.sockserver
+        self.connectors[self.sockserver] = 'server'
         print('Chatserver started on port {}'.format(self.port))
 
     def run(self):
         while True:
             read_sockets, write_sockets, error_sockets = select.select(
-                self.connectors.values(), [], [])
+                self.connectors.keys(), [], [])
 
             for sock in read_sockets:
 
@@ -33,39 +33,43 @@ class ChatServer:
                         status = 'Client left {}:{}\r\n'.format(host, port)
                         self.broadcast_string(status, sock)
                         sock.close()
-                        for k, v in self.connectors.items():
-                            if v == sock:
-                                del self.connectors[k]
+                        del self.connectors[sock]
                     else:
-                        host, port = sock.getpeername()
-                        newstr = '[{}:{}] {}'.format(host, port, data)
-                        self.broadcast_string(newstr, sock)
+                        # host, port = sock.getpeername()
+                        _user = self.connectors[sock]
+                        msg = data.decode('utf-8')
+                        if msg.find(':') != -1:
+                            for k, v in self.connectors.items():
+                                if v == msg.split(':')[0]:
+                                    _to = k
+                                    break
+                            self.send_message(msg.split(':')[1], _to, _user)
+                        else:
+                            newstr = '{}'.format(data)
+                            self.broadcast_string(newstr, sock)
 
     def accept_new_connection(self):
         newsock, (remhost, remport) = self.sockserver.accept()
         username = newsock.recv(1024).decode('utf-8')
-        self.connectors[username] = newsock
-        newsock.send(bytes("You're connected to the chatserver\r\n", 'utf-8'))
-        status = 'Client joined {}:{}\r\n'.format(remhost, remport)
+        self.connectors[newsock] = username
+        # newsock.send(bytes("You're connected to the chatserver\r\n", 'utf-8'))
+        status = '{} is online \r\n'.format(username)
         self.broadcast_string(status, newsock)
 
     def broadcast_string(self, msg, omit_sock):
-        for sock in self.connectors.values():
+        for sock in self.connectors.keys():
             if sock != self.sockserver and sock != omit_sock:
                 try:
                     sock.send(bytes(msg, 'utf-8'))
                 except:
                     sock.close()
-                    for k, v in self.connectors.items():
-                        if v == sock:
-                            del self.connectors[k]
+                    del self.connectors[sock]
 
-    def send_message(self, msg, receiver):
-        host = self.sockserver.getpeername()[0]
-        modified_msg = '[{} says ] {}'.format(host, msg)
-        receiver.send(modified_msg)
+    def send_message(self, msg, receiver, sender):
+        modified_msg = '{} > {}'.format(sender, msg)
+        receiver.send(bytes(modified_msg, 'utf-8'))
 
 
 if __name__ == '__main__':
-    schat = ChatServer(5900)
+    schat = ChatServer(5400)
     schat.run()
